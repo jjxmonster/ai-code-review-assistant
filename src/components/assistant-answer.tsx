866fcce896863bc7b0c455cb745b78129c8de30a
jjxmonster <kuba.tarabasz@wp.pl>
2024-04-04 00:01:15 +0200

    Display model answer dynamically

--- src/components/assistant-answer.tsx
+++ src/components/assistant-answer.tsx
@@ -1,12 +1,20 @@
 import { useAppStore } from "@/store/store";
 import { Card } from "./ui/card";
+import { parse } from "marked";
 
 export const AssistantAnswer = () => {
 	const { modelAnswer } = useAppStore();
 
+	const createMarkup = () => {
+		return { __html: parse(modelAnswer) };
+	};
+
 	return (
-		<Card className="p-2 text-white bg-slate-800 w-full max-w-5xl border-0">
-			{modelAnswer}
+		<Card className="text-white bg-slate-900 w-full max-w-5xl border-0 p-10">
+			<div
+				className="prose-invert prose-lg prose text-start max-w-full"
+				dangerouslySetInnerHTML={createMarkup()}
+			/>
 		</Card>
 	);
 };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
-export const systemMessage = `As an AI Code Review Assistant, your primary task is to analyze frontend code submissions for quality, efficiency, and adherence to best practices. Start by determining if the submission is JavaScript or TypeScript code. If the submission is not JavaScript, TypeScript, or doesn’t appear to be code at all, respond with ‘This is not code that I can review.’
+export const systemMessage = `As an AI Code Review Assistant, your primary task is to analyze frontend code submissions for quality, efficiency, and adherence to best practices. Start by determining if the submission is JavaScript or TypeScript code. If the submission is not JavaScript, TypeScript, or doesn’t appear to be code at all, respond with ‘This is not a code that I can review.’
 
 When reviewing valid code, consider the following areas:
 
@@ -15,4 +15,7 @@ When reviewing valid code, consider the following areas:
 - Security: Highlight security vulnerabilities and propose remedies.
 - Accessibility: Ensure the code meets accessibility standards.
 - Responsiveness: Verify the UI’s adaptability across different devices and screen sizes.
-- Provide actionable feedback, highlighting specific issues and suggesting clear, concise fixes. Engage in a friendly, supportive manner to encourage learning and improvement.`;
+- Provide actionable feedback, highlighting specific issues and suggesting clear, concise fixes. Engage in a friendly, supportive manner to encourage learning and improvement.
+
+Answer should be in Markdown format without ${"```markdown"} at the beginning.
+`;
