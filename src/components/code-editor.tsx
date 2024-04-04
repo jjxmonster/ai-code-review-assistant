747133ec3442ccfaa579799f997e9093133b61d4
jjxmonster <kuba.tarabasz@wp.pl>
2024-04-04 21:08:30 +0200

    Favicon, styles and small fixes

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -6,7 +6,7 @@ import { Card } from "./ui/card";
 export const CodeEditor = () => {
 	const { code, setCode } = useAppStore();
 	return (
-		<Card className="p-2 bg-slate-800 h-[500px] w-full max-w-5xl border-0">
+		<Card className="p-2 bg-slate-900 h-[500px] w-full max-w-5xl border-0">
 			<Editor
 				defaultLanguage="typescript"
 				theme="vs-dark"
