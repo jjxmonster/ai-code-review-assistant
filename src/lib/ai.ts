6d4a2509f0a0a5d242397cefb0e95aec73cc5bb8
jjxmonster <kuba.tarabasz@wp.pl>
2024-05-11 18:33:05 +0200

    Implemented GPT-3.5 Guardrail

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -2,6 +2,8 @@ import { ChatOpenAI } from "@langchain/openai";
 import { HumanMessage, SystemMessage } from "langchain/schema";
 import { useAppStore } from "@/store/store";
 import { systemMessage } from "./utils";
+import { PromptTemplate } from "langchain/prompts";
+import { LLMChain } from "langchain/chains";
 
 export const getCodeReview = async () => {
 	const { code, setModelAnswer } = useAppStore.getState();
@@ -16,16 +18,31 @@ export const getCodeReview = async () => {
 		modelName: "gpt-4-turbo-preview",
 	});
 
-	await chat.invoke(
-		[new SystemMessage(systemMessage), new HumanMessage(code)],
-		{
-			callbacks: [
-				{
-					handleLLMNewToken(token: string) {
-						setModelAnswer(prevState => prevState + token);
+	const guardRail = new ChatOpenAI({
+		modelName: "gpt-3.5-turbo",
+		openAIApiKey: import.meta.env.VITE_OPENAI_API_KEY,
+	});
+	const guardPrompt = `Return 1 or 0 if the code: {code} is a valid code snippet`;
+	const prompt = PromptTemplate.fromTemplate(guardPrompt);
+	const chain = new LLMChain({ llm: guardRail, prompt });
+	const { text } = await chain.call({
+		code,
+	});
+
+	if (text === "1") {
+		await chat.invoke(
+			[new SystemMessage(systemMessage), new HumanMessage(code)],
+			{
+				callbacks: [
+					{
+						handleLLMNewToken(token: string) {
+							setModelAnswer(prevState => prevState + token);
+						},
 					},
-				},
-			],
-		}
-	);
+				],
+			}
+		);
+	} else {
+		setModelAnswer("Invalid code snippet");
+	}
 };
